42a78369f48ae59d4b12dbd7fcd68740215d10e4
Thong Do <tdo05824@gmail.com>
2019-11-09 21:55:07 +0700

    destructed props arguments from table.jsx, listgroup.jsx, pagination.jsx

--- src/components/common/listGroup.jsx
+++ src/components/common/listGroup.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 
-const ListGroup = props => {
-  const {
-    items,
-    valueProperty,
-    textProperty,
-    onItemSelect,
-    selectedItem
-  } = props;
-
+const ListGroup = ({
+  items,
+  valueProperty,
+  textProperty,
+  onItemSelect,
+  selectedItem
+}) => {
   return (
     <ul className="list-group">
       {items.map(item => (
