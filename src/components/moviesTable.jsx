2fbf1807a2e4e3e5827168f47ce84200fb7b03f3
Thong Do <tdo05824@gmail.com>
2019-11-09 20:32:41 +0700

    implementing renderCell

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -9,8 +9,23 @@ class MoviesTable extends Component {
     { path: "genre.name", label: "Genre" },
     { path: "numberInStock", label: "Stock" },
     { path: "dailyRentalRate", label: "Rate" },
-    { key: "like" },
-    { key: "delete" }
+    {
+      key: "like",
+      content: movie => (
+        <Like onClick={() => this.props.onLike(movie)} liked={movie.liked} />
+      )
+    },
+    {
+      key: "delete",
+      content: movie => (
+        <button
+          onClick={() => this.props.onDelete(movie)}
+          className="btn btn-danger btn-sm"
+        >
+          Delete
+        </button>
+      )
+    }
   ];
 
   render() {
@@ -24,27 +39,6 @@ class MoviesTable extends Component {
           onSort={onSort}
         />
         <TableBody columns={this.columns} data={movies} />
-        {/* <tbody>
-          {movies.map(movie => (
-            <tr key={movie._id}>
-              <td>{movie.title}</td>
-              <td>{movie.genre.name}</td>
-              <td>{movie.numberInstock}</td>
-              <td>{movie.dailyRentalRate}</td>
-              <td>
-                <Like onClick={() => onLike(movie)} liked={movie.liked} />
-              </td>
-              <td>
-                <button
-                  onClick={() => onDelete(movie)}
-                  className="btn btn-danger btn-sm"
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody> */}
       </table>
     );
   }
