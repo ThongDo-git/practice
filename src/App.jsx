ecdc128bb82e0a1b9c959f63ffec171dc31ad309
Thong Do <tdo05824@gmail.com>
2019-11-11 00:40:17 +0700

    renamed App.js to App.jsx, added customers movieForm notFound rentals

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import Movies from "./components/movies";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <main className="container">
+        <Route path="/movies" component={Movies}></Route>
+        <Route path="/customers" component={Customers}></Route>
+        <Route path="/rentals" component={Rentals}></Route>
+        <Route path="/not-found" component={NotFound}></Route>
+      </main>
+    );
+  }
+}
+
+export default App;
