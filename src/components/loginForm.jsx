d5f88ec0cc958cba4b011b88aeb99971cb2ec5a1
Thong Do <tdo05824@gmail.com>
2019-11-12 23:01:47 +0700

    added state in loginForm

---
+++ src/components/loginForm.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+
+class LoginForm extends Component {
+  state = {
+    account: { username: "", password: "" }
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    // Call the server
+    console.log("Submitted");
+  };
+
+  hangleChange = e => {
+    const account = { ...this.state.account };
+    account.username = e.currentTarget.value;
+    this.setState({ account });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Login Form</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input
+              autoFocus
+              value={this.state.account.username}
+              onChange={this.handleChange}
+              id="username"
+              type="text"
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              autoFocus
+              id="password"
+              type="text"
+              className="form-control"
+            />
+          </div>
+          <button className="btn btn-primary">Login</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
